4112c2a8b6b84bced2f8659f71aab2dcbbcc378f
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2025-11-09 16:55:57 +0000

    Bugfix Accessioning

--- whg/context_processors.py
+++ whg/context_processors.py
@@ -6,8 +6,9 @@ from django.conf import settings
 def environment(request):
     return {
         'environment': os.getenv('ENV_CONTEXT', 'default'),
+        'ENV_CONTEXT': os.getenv('ENV_CONTEXT', 'default'),
         'GLITCHTIP_DSN': settings.GLITCHTIP_DSN,
-        'GLITCHTIP_RELEASE': settings.GLITCHTIP_RELEASE
+        'GLITCHTIP_RELEASE': settings.GLITCHTIP_RELEASE,
     }
 
 def app_version(request):
